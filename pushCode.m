e20b74448ed00da67e95e8cc87200a837b0aa74e
rooohan <rooohan@Pactera.com>
2021-01-06 11:20:05 +0800

    delete

--- pushCode.m
+++
@@ -1,5 +0,0 @@
-%----------自动提交代码-----------%
-% !git init
-!git add --all
-!git commit -m "auto save"
-!git push origin main
--- saveModel.m
+++
@@ -1,7 +0,0 @@
-function saveModel()
-    model_name = "subtract";    
-%     open_system(model_name);
-    load_system(model_name);
-    save_system(model_name);
-    close_system(model_name);
-end
