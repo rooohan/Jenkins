25edfc7a5fd43da633d8cd8c11eb33a967c56cff
Hannahnn <1079062131@qq.com>
2021-01-06 10:01:17 +0800

    modify

---
+++ runTheseTests.m
@@ -0,0 +1,13 @@
+function runTheseTests()
+    %RUNTESTS 
+
+    %   Copyright 2018 MathWorks, Inc.
+
+    project = matlab.project.currentProject;
+    % The runtests function provides a simple way to run a collection of
+    % tests
+    disp(project.RootFolder);
+    result = runtests(project.RootFolder);
+    
+end
+
