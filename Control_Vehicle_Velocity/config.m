c5870807cc57ccfc6d0bb7b64f6d9a3a1eab5f04
Hannahnn <1079062131@qq.com>
2021-03-02 09:15:43 +0800

    modify

--- Control_Vehicle_Velocity/config.m
+++ Control_Vehicle_Velocity/config.m
@@ -43,6 +43,7 @@ cs.set_param('AutoInsertRateTranBlk', 'off');   % Automatically handle rate tran
 % Data Import/Export
 cs.set_param('Decimation', '1');   % Decimation
 cs.set_param('LoadExternalInput', 'off');   % Load external input
+cs.set_param('ExternalInput', '[0, input]');   % External input variable
 cs.set_param('SaveFinalState', 'off');   % Save final state
 cs.set_param('LoadInitialState', 'off');   % Load initial state
 cs.set_param('LimitDataPoints', 'off');   % Limit data points
